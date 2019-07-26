ac0dae125b6424209b7cff0c8ec141cc8be4f402
Infant Frontender <newbornfrontender@gmail.com>
2019-07-26 18:05:28 +0300

    feat: add postcss-import

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,6 +1,7 @@
 module.exports = ({ production }) => ({
   plugins: {
     'postcss-use': true,
+    'postcss-import': true,
     'postcss-preset-env': {
       stage: 3,
       autoprefixer: production,
