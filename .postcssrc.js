928b73351d74f4b54dabd60bdc67ac2a8234943f
Infant Frontender <newbornfrontender@gmail.com>
2019-07-26 18:10:17 +0300

    feat: add postcss-normalize

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,6 +2,7 @@ module.exports = ({ production }) => ({
   plugins: {
     'postcss-use': true,
     'postcss-import': true,
+    'postcss-normalize': true,
     'postcss-preset-env': {
       stage: 3,
       autoprefixer: production,
