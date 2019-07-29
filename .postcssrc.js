77e4475a480fa382f9757de621cf3b6b36b6f8d9
Infant Frontender <newbornfrontender@gmail.com>
2019-07-29 09:46:46 +0300

    chore: repove postcss-normalize

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,7 +2,6 @@ module.exports = ({ production }) => ({
   plugins: {
     'postcss-use': true,
     'postcss-import': true,
-    'postcss-normalize': true,
     'postcss-preset-env': {
       stage: 3,
       autoprefixer: production,
